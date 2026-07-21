7fb2c90d19d60ca8cb6fb9bb3755008a07f7988d
DEEPADHARSHAN-N <deepa007dharshan007@gmail.com>
2026-07-21 14:26:45 +0530

    commit

---
+++ numberpy.c
@@ -0,0 +1,24 @@
+int main () {
+
+    int i , j , k = 1;
+
+    for(i=1;i<=5;i++){
+
+        for(j=1;j<=5-i;j++){
+
+            printf("    ");
+        }
+
+        for(j=1;j<=2*i-1;j++){
+
+            printf(" %d ",k);
+            k++;
+        }
+
+        printf(" \n ");
+
+    }
+
+    return 0;
+
+}
